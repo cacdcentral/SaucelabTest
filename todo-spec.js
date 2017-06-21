ede7cab73cea3cc6a7e273409eaf6915d1616294
cacdcentral <gustavo.chavez@ca.com>
2017-06-21 15:39:47 -0500

    Update 7-21-2017
    Changes from Keith Puzey

--- todo-spec.js
+++ todo-spec.js
@@ -2,7 +2,7 @@ describe('CDBU Modern Software Factory UI Test - xxx Environment', function() {
   it('should add a todo', function() {
     browser.ignoreSynchronization = true;
     browser.get('http://<preprod_url>');
-    element(by.css('[name="Name"]')).sendKeys('Selenium Test User');
-    element(by.buttonText('Submit')).click();
+    element(by.css('[name="name"]')).sendKeys('Selenium Test User');
+    element(by.buttonText('I\'m In!')).click();
     });
 });
